181c0f0de9c321bf7228cd0fba4588688222aff5
zxb5102 <1156594221@qq.com>
2018-06-17 18:00:37 +0800

    'init'

---
+++ src/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    <h1 class="msg">
+        {{msg}}
+        {{testDataFetch}}
+    </h1>
+    </div>   
+</template>
+<script>
+export default {
+  asyncData({ store }) {
+    return store.dispatch("getData");
+  },
+  data() {
+    msg: "test";
+  },
+  computed: {
+    testDataFetch() {
+      return this.$store.state.num;
+    }
+  }
+};
+</script>
+<style lang="less">
+.msg{
+    color:red;
+}
+</style>
+
+
+
